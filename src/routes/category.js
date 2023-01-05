5afa0c55d002d4a00edbd9d046d65d1dfe26c72f
Cristian Sorrentino <tradolcesalatoinfo@gmail.com>
2023-01-05 19:31:05 +0100

    modificato file category.js

--- src/routes/category.js
+++ src/routes/category.js
@@ -6,7 +6,22 @@ const categories = require('../db/db')
 
 
 category.get('/api/categories', (req, res) => {
-  res.json(categories)
+  res.json([{
+    title : "Masse Montate",
+    image : "https://www.misya.info/wp-content/uploads/2018/08/pan-di-spagna.jpg"
+},
+{
+    title : "Pasta Frolla",
+    image : "https://www.giallozafferano.it/images/173-17338/Pasta-frolla_450x300.jpg"
+},
+{
+    title : "Biscotti",
+    image: "https://www.ricettedellanonna.net/wp-content/uploads/2022/01/biscotti-al-burro-9-620x414.jpg"
+},
+{
+    title : "Crostate",
+    image : "https://www.lucake.it/wp-content/uploads/2022/09/crostata-di-marmellata.jpg"
+}])
 })
 
 module.exports = category;
